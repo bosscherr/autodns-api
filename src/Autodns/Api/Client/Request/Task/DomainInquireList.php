f2dd003f09b5d71a8c2cbf600d77fed2a4e5bc4a
Rick Bosscher <rickbosscher@gmail.com>
2015-05-06 16:36:14 +0200

    Created an InquireList and made the Domain- and Handle extensions of it

--- src/Autodns/Api/Client/Request/Task/DomainInquireList.php
+++ src/Autodns/Api/Client/Request/Task/DomainInquireList.php
@@ -5,87 +5,17 @@ namespace Autodns\Api\Client\Request\Task;
 
 use Autodns\Api\Client\Request\Task;
 
-class DomainInquireList implements Task
+class DomainInquireList extends InquireList
 {
-    /**
-     * @var string[]
-     */
-    private $keys;
-    /**
-     * @var QueryInterface
-     */
-    private $query;
 
-    /**
-     * @var string[]
-     */
-    private $view;
-    
-    /**
-     * @var string[]
-     */
-    private $order;
-    
+  
     public function asArray() {
-        $array = array('code' => '0105');
-
-        if ($this->view) {
-            $array['view'] = $this->view;
-        }
-
-        if ($this->keys) {
-            $array['key'] = $this->keys;
-        }
-
-        if ($this->order) {
-          $array['order'] = $this->order;
-        }
-
-        if ($this->query) {
-            $array['where'] = $this->query->asArray();
-        }
+        $array = parent::asArray();
+        $array['code'] = '0105';
+    
 
         return $array;
     }
 
-    /**
-     * @param $view
-     * @return $this
-     */
-    public function withView(array $view)
-    {
-        $this->view = $view;
-        return $this;
-    }
-
-    /**
-     * @param array $keys
-     * @return $this
-     */
-    public function withKeys(array $keys)
-    {
-        $this->keys = $keys;
-        return $this;
-    }
-
-    /**
-     * @param QueryInterface $query
-     * @return $this
-     */
-    public function withQuery(QueryInterface $query)
-    {
-        $this->query = $query;
-        return $this;
-    }
-
-    /**
-     * @param $order
-     * @return $this
-     */
-    public function withOrder(array $order)
-    {
-      $this->order = $order;
-      return $this;
-    }
-
+ 
 }
